b9204bc4ade0d334ca02174f3c821467085361b7
crypt096 <acvjetan@gmail.com>
2021-01-17 11:24:17 +0100

    Database setup.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import routes from './src/routes/routes'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 
 const app = express();
 const PORT = 4000;
+
+// Mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/alexdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// Bodyparser setup
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 
 routes(app);
 
